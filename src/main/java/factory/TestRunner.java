10e99fe28ff6ca0dddb0feb28b0fbdd0756e6504
GresiXhavara1 <gresi.xhavara@fshnstudent.info>
2019-04-03 10:09:18 +0200

    init

--- src/main/java/factory/TestRunner.java
+++ src/main/java/factory/TestRunner.java
@@ -1,13 +1,15 @@
 package factory;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
-import org.junit.runner.RunWith;
+        import cucumber.api.CucumberOptions;
+        import cucumber.api.junit.Cucumber;
+        import org.junit.runner.RunWith;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/main/resources/features",
-        monochrome = true
+        monochrome = true,
+        glue = "steps/RegisterAndLogin",
+        tags = {"@Test1"}
 )
-public class TestRunner {
+public class TestRunner{
 }
